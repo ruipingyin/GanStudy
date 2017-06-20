292c505a96883a3b26a725af40998b979208c5e3
Ruiping Yin <zzrabs@gmail.com>
2017-06-20 23:01:02 +1000

    start gan recommender system

--- run.py
+++ run.py
@@ -27,21 +27,21 @@ def createGenerativeNetwork():
     with tf.name_scope('Layer1') as scope:
       w = tf.Variable(tf.random_normal([LEN, 32]))
       b = tf.Variable(tf.zeros([1, 32]) + 0.1)
-      out = tf.nn.relu(tf.matmul(z, w) + b)
+      out = tf.nn.relu(tf.add(tf.matmul(z, w), b))
       Params.append(w)
       Params.append(b)
     
     with tf.name_scope('Layer2') as scope:
       w = tf.Variable(tf.random_normal([32, 32]))
       b = tf.Variable(tf.zeros([1, 32]) + 0.1)
-      out = tf.nn.sigmoid(tf.matmul(out, w) + b)
+      out = tf.nn.sigmoid(tf.add(tf.matmul(out, w), b))
       Params.append(w)
       Params.append(b)
     
     with tf.name_scope('Layer3') as scope:
       w = tf.Variable(tf.random_normal([32, LEN]))
       b = tf.Variable(tf.zeros([1, LEN]) + 0.1)
-      out = tf.matmul(out, w) + b
+      out = tf.add(tf.matmul(out, w), b)
       Params.append(w)
       Params.append(b)
 
@@ -50,24 +50,30 @@ def createGenerativeNetwork():
 def createDiscriminatorNetwork():
   ''' Discriminator Network '''
   with tf.name_scope('Discriminator') as scope:
-    Weights = tf.Variable(tf.random_normal([2, 32]), name = 'D_W')
-    Bias = tf.Variable(tf.zeros([1, 32]) + 0.1, name = 'D_B')
-    DOutput = tf.matmul(x, Weights) + Bias
-    DOutput = tf.nn.relu(DOutput)
+    Params = []
+  
+    with tf.name_scope('Layer1') as scope:
+      w = tf.Variable(tf.random_normal([2, 32]), name = 'D_W')
+      b = tf.Variable(tf.zeros([1, 32]) + 0.1, name = 'D_B')
+      out = tf.nn.relu(tf.add(tf.matmul(x, w), b))
+      Params.append(w)
+      Params.append(b)
     
-    Weights2 = tf.Variable(tf.random_normal([32, 32]), name = 'D_W2')
-    Bias2 = tf.Variable(tf.zeros([1, 32]) + 0.1, name = 'D_B2')
-    DOutput2 = tf.matmul(DOutput, Weights2) + Bias2
-    DOutput2 = tf.nn.sigmoid(DOutput2)
+    with tf.name_scope('Layer2') as scope:
+      w = tf.Variable(tf.random_normal([32, 32]), name = 'D_W2')
+      b = tf.Variable(tf.zeros([1, 32]) + 0.1, name = 'D_B2')
+      out = tf.nn.sigmoid(tf.add(tf.matmul(out, w), b))
+      Params.append(w)
+      Params.append(b)
     
-    Weights3 = tf.Variable(tf.random_normal([32, 1]), name = 'D_W3')
-    Bias3 = tf.Variable(tf.zeros([1, 1]) + 0.1, name = 'D_B3')
-    DOutput3_ = tf.matmul(DOutput2, Weights3) + Bias3
-    # DOutput3 = tf.nn.sigmoid(DOutput3_)
+    with tf.name_scope('Layer3') as scope:
+      w = tf.Variable(tf.random_normal([32, 1]), name = 'D_W3')
+      b = tf.Variable(tf.zeros([1, 1]) + 0.1, name = 'D_B3')
+      out = tf.add(tf.matmul(out, w), b)
+      Params.append(w)
+      Params.append(b)
     
-    DParams = [Weights, Bias, Weights2, Bias2, Weights3, Bias3]
-    
-  return (DParams, DOutput3_)
+  return (Params, out)
 
 def createGenerativeAdversarialNetwork(GOutput3):
   ''' Generative Adversarial Network '''
@@ -75,27 +81,28 @@ def createGenerativeAdversarialNetwork(GOutput3):
     Mean = tf.reduce_mean(GOutput3, 1)
     MeanT = tf.transpose(tf.expand_dims(Mean, 0))
     Std = tf.sqrt(tf.reduce_mean(tf.square(GOutput3 - MeanT), 1))
-    Data = tf.concat(1, [MeanT, tf.transpose(tf.expand_dims(Std, 0))])  # mean and std of GN out
+    [MeanT, tf.transpose(tf.expand_dims(Std, 0))]
+    Data = tf.concat([MeanT, tf.transpose(tf.expand_dims(Std, 0))], 1)  # mean and std of GN out
     GANParams = []
     
     with tf.name_scope('Layer1') as scope:
       w = tf.Variable(tf.random_normal([2, 32]), name = 'GAN_W1')
       b = tf.Variable(tf.zeros([1, 32]), name = 'GAN_B1')
-      out = tf.nn.relu(tf.matmul(Data, w) + b)
+      out = tf.nn.relu(tf.add(tf.matmul(Data, w), b))
       GANParams.append(w)
       GANParams.append(b)
     
     with tf.name_scope('Layer2') as scope:
       w = tf.Variable(tf.random_normal([32, 32]), name = 'GAN_W2')
       b = tf.Variable(tf.zeros([1, 32]), name = 'GAN_B2')
-      out = tf.nn.sigmoid(tf.matmul(out, w) + b)
+      out = tf.nn.sigmoid(tf.add(tf.matmul(out, w), b))
       GANParams.append(w)
       GANParams.append(b)
     
     with tf.name_scope('Layer3') as scope:
       w = tf.Variable(tf.random_normal([32, 1]), name = 'GAN_W3')
       b = tf.Variable(tf.zeros([1, 1]) + 0.1, name = 'GAN_B3')
-      out = tf.matmul(out, w) + b
+      out = tf.add(tf.matmul(out, w), b)
       GANParams.append(w)
       GANParams.append(b)
   
@@ -105,31 +112,31 @@ if __name__ == '__main__':
   x = tf.placeholder(tf.float32, shape = [None, 2], name = 'feature')  # Mean and std
   y = tf.placeholder(tf.float32, shape = [None, 1], name = 'label')
   
-  z, GParams, GOutput3 = createGenerativeNetwork()
-  DParams, DOutput3_ = createDiscriminatorNetwork()
-  GANParams, out = createGenerativeAdversarialNetwork(GOutput3)
+  z, GParams, Gout = createGenerativeNetwork()
+  DParams, Dout = createDiscriminatorNetwork()
+  GANParams, out = createGenerativeAdversarialNetwork(Gout)
   
   ''' Loss Functions '''
-  DLoss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(DOutput3_, y))
-  GLoss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(out, y))
+  DLoss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = Dout, labels = y))
+  GLoss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits = out, labels = y))
 
   ''' Optimizer '''
   DOpertimizer = tf.train.GradientDescentOptimizer(0.01).minimize(DLoss, global_step = tf.Variable(0), var_list = DParams)
   GOpertimizer = tf.train.GradientDescentOptimizer(0.05).minimize(GLoss, global_step = tf.Variable(0), var_list = GParams)
-
+ 
   ''' Train '''
   dLossHistory = []
   gLossHistory = []
   
   with tf.Session() as sess:
-    sess.run(tf.initialize_all_variables())
+    sess.run(tf.global_variables_initializer())
     
     for step in range(100):
       # Train discrimination network
       for _ in range(100):
         real = sampleData(100, length = LEN)
         noise = randomData(100, length = LEN)
-        generate = sess.run(GOutput3, feed_dict = {z:noise}) # generate samples
+        generate = sess.run(Gout, feed_dict = {z:noise}) # generate samples
         xBatch = preprocessData(list(real) + list(generate))
         yBatch = [[1] for _ in range(len(real))] + [[0] for _ in range(len(generate))]
         dLossValue, _ = sess.run([DLoss, DOpertimizer], feed_dict = {x:xBatch, y:yBatch})
@@ -146,7 +153,7 @@ if __name__ == '__main__':
       
       if step % 10 == 0 or step + 1 == 100:
         noise = randomData(1, length = LEN)
-        generate = sess.run(GOutput3, feed_dict = {z:noise})
+        generate = sess.run(Gout, feed_dict = {z:noise})
         print "[%4d] GAN-d-loss: %.12f GAN-g-loss: %.12f generate-mean: %.4f generate-std: %.4f" % (step, dLossValue, gLossValue, generate.mean(), generate.std())
 
   plt.figure(1)
@@ -155,7 +162,7 @@ if __name__ == '__main__':
   plt.plot(dLossHistory)
   plt.subplot(212)
   plt.plot(gLossHistory,c="g")
-  plt.savefig('./M1.jpg')
+  plt.savefig('./M1.png')
 
   plt.figure(2)
   real = sampleData(1,length = LEN)
@@ -167,4 +174,4 @@ if __name__ == '__main__':
 
   (data, bins) = np.histogram(generate[0])
   plt.plot(bins[:-1], data, c="r")
-  plt.savefig('./M2.jpg')
+  plt.savefig('./M2.png')
